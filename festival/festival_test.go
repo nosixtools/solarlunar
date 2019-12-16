8fd50b36b07b2f6dc2bb18c3e30c3bc25bb3d787
heyaxiang <heyaxiang@didiglobal.com>
2019-12-16 18:30:32 +0800

    修复#2问题

--- festival/festival_test.go
+++ festival/festival_test.go
@@ -13,5 +13,8 @@ func TestGetFestivals(t *testing.T) {
 	fmt.Println(festival.GetFestivals("2017-04-04"))
 	fmt.Println(festival.GetFestivals("2017-04-03"))
 	fmt.Println(festival.GetFestivals("2018-02-15"))
-	fmt.Println(festival.GetFestivals("2018-02-16"))
+	fmt.Println(festival.GetFestivals("2019-11-28"))
+	fmt.Println(festival.GetFestivals("2019-09-22"))
+	fmt.Println(festival.GetFestivals("2020-11-26"))
+	fmt.Println(festival.GetFestivals("2020-09-27"))
 }
